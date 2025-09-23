e3f137301460f9a8b9ea46c0735bd4871ba4b8d3
Fernandkamdem17 <fernandkamdem7@gmail.com>
2025-09-23 12:26:52 +0200

    Premier commit de developpement avec toutes les routes CRUD des modules au propre

--- Modules/Actors/routes/web.php
+++ Modules/Actors/routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Actors\Http\Controllers\ActorsController;
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::prefix('actors')->middleware(['auth', 'verified'])->group(function () {
     Route::resource('actors', ActorsController::class)->names('actors');
 });
--- Modules/Shippings/routes/web.php
+++ Modules/Shippings/routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Shippings\Http\Controllers\ShippingsController;
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::prefix('shippings')->middleware(['auth', 'verified'])->group(function () {
     Route::resource('shippings', ShippingsController::class)->names('shippings');
 });
